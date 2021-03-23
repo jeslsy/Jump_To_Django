56975d20348430023f44aad31730dcfe8b176e08
jeslsy <jeslsy0507@naver.com>
2021-03-23 14:57:53 +0900

    create paging

--- pybo/views.py
+++ pybo/views.py
@@ -5,14 +5,32 @@ from django.shortcuts import redirect, render, get_object_or_404
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+# 페이징 기능 
+from django.core.paginator import Paginator
+
+
 
 # request = 자동으로 전달되는 HTTP 요청 객체
 def index(request):
-    # pybo 목록 출력
+    """
+    pybo 목록 출력
+    """
+    # 입력 파라미터
+    # 여기서 1은 /pybo/같이 page 파라미터 없는 것을 위한 디폴트 값
+    page = request.GET.get('page','1') # 페이지
+    # 조회
     question_list =Question.objects.order_by('-create_date')
+    
+    # 페이징 처리
+    # 페이징 구현에 사용한 클래스 = Panginator = question_list를 페이징 객체 paginator로 변환
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
     # question_list를 템플릿에 전달
-    context = {'question_list':question_list}
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
+
+
+
 
 
 def detail(request, question_id):
